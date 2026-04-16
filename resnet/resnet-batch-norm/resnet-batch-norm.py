81ffd29e2fa88813cf644ab8787559ea6e8f979e
Phạm Hữu Nam <just.study.for.live@gmail.com>
2026-04-16 22:28:58 +0700

    Add solution for resnet-batch-norm

---
+++ resnet/resnet-batch-norm/resnet-batch-norm.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def ReLU(x):
+    return np.maximum(x, 0)
+
+def batch_norm_block(x, W1, W2, gamma1, beta1, gamma2, beta2, mode):
+    """
+    Returns: np.ndarray of same shape as input with batch-normalized and skip-connected output
+    """
+    x = np.array(x)
+    W1 = np.array(W1)
+    W2 = np.array(W2)
+    
+    # YOUR CODE HERE
+    if mode == "post":
+        shortcut = x
+        x = x @ W1
+        
+        mean = np.mean(x, axis = 0)
+        var = np.var(x, axis = 0)
+        x_hat = (x - mean) / np.sqrt(var + 1e-5)
+        x = x_hat * gamma1 + beta1
+
+        x = ReLU(x)
+        x = x @ W2
+
+        mean = np.mean(x, axis = 0)
+        var = np.var(x, axis = 0)
+        x_hat = (x - mean) / np.sqrt(var + 1e-5)
+        x = x_hat * gamma2 + beta2 
+
+        x = x + shortcut 
+        x = ReLU(x)
+
+    elif mode == 'pre':
+        shortcut = x
+        
+        mean = np.mean(x, axis = 0)
+        var = np.var(x, axis = 0)
+        x_hat = (x - mean) / np.sqrt(var + 1e-5)
+        x = x_hat * gamma1 + beta1
+
+        x = ReLU(x) 
+
+        x = x @ W1
+        
+        mean = np.mean(x, axis = 0)
+        var = np.var(x, axis = 0)
+        x_hat = (x - mean) / np.sqrt(var + 1e-5)
+        x = x_hat * gamma2 + beta2 
+
+        x = ReLU(x)
+
+        x = x @ W2
+        
+        x = x + shortcut
+    res_rounded = np.round(x, 4).tolist()
+    return {'output' : res_rounded, 'mode': mode}
