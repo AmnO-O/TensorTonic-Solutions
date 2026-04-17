978743ed12a47d405cb35d0792ef1b2c688f0932
Phạm Hữu Nam <just.study.for.live@gmail.com>
2026-04-17 15:25:52 +0700

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -5,24 +5,29 @@ def mean_median_mode(x):
     """
     Compute mean, median, and mode.
     """
-    if not x:
-        return None
-        
+    # Write code here
     x = sorted(x)
     n = len(x)
     
-    # 1. Tính Mean
-    mean = sum(x) / n
-    
-    # 2. Tính Median
+    if n == 0:
+        return None 
+        
+    mean = 0 
+    mode = 0 
+    median = 0 
+    counts = {}
+
+    for val in x: 
+        mean = mean + val
+        counts[val] = counts.get(val, 0) + 1
+
     if n % 2 == 1:
         median = x[n // 2]
     else:
-        median = (x[n // 2 - 1] + x[n // 2]) / 2.0
+        median = (x[n // 2 - 1] + x[n // 2]) / 2
         
-    # 3. Tính Mode
-    counts = Counter(x)
-    mode = max(counts, key=counts.get)
-    
+    mode = max(counts, key = counts.get)    
+    mean = mean / n 
 
-    return mean, median, mode
+    
+    return mean, median, mode 
