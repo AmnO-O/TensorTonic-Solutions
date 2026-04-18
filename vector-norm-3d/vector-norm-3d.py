4bba9fa20f07a117f0d5765891a90cbd2e91157d
Phạm Hữu Nam <just.study.for.live@gmail.com>
2026-04-18 07:21:20 +0700

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -7,8 +7,9 @@ def vector_norm_3d(v):
     # Your code here
     v = np.array(v)
     is_single = v.ndim == 1 
-    if v.ndim == 1:
-        v = v[np.newaxis, : ]
+
+    if v.ndim == 1: 
+        v = np.expand_dims(v, 0)
   
     norm = np.sqrt(np.sum(v ** 2, axis = 1))
     
