55fef6955641dc609677cb1c660cd30c49fb202d
Phạm Hữu Nam <just.study.for.live@gmail.com>
2026-04-19 08:22:35 +0700

    Update solution for vgg-maxpool

--- vgg/vgg-maxpool/vgg-maxpool.py
+++ vgg/vgg-maxpool/vgg-maxpool.py
@@ -6,12 +6,7 @@ def vgg_maxpool(x: np.ndarray) -> np.ndarray:
     """
     # Your implementation here
     N, H, W, C = x.shape 
-    out = np.zeros((N, H // 2, W // 2, C))
-    
-    for i in range(0, H, 2): 
-        for j in range(0, W, 2): 
-            window = x[: , i : i + 2, j : j + 2, :]
-            out[: , i // 2, j // 2, :] = np.max(window, axis = (1, 2)) 
+    out = x.reshape(N, H // 2, 2, W // 2, 2, C)
 
-    return out 
+    return out.max(axis = (2, 4))
     
