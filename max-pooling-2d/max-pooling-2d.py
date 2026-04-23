f81fb66950bb691552b1976e6b6d9ad55b3ca9f9
Phạm Hữu Nam <just.study.for.live@gmail.com>
2026-04-23 20:13:07 +0700

    Update solution for max-pooling-2d

--- max-pooling-2d/max-pooling-2d.py
+++ max-pooling-2d/max-pooling-2d.py
@@ -10,13 +10,6 @@ def max_pooling_2d(X, pool_size):
     H_out = H // pool_size 
     W_out = W // pool_size 
 
-    out = np.zeros((H_out, W_out))
+    x = x.reshape(H_out, pool_size, W_out, pool_size)
 
-    for i in range(H_out):
-        for j in range(W_out):
-            # Cắt vùng cửa sổ từ (i*s) đến ((i+1)*s)
-            window = x[i*pool_size : (i+1)*pool_size, 
-                       j*pool_size : (j+1)*pool_size]
-            out[i, j] = np.max(window) 
-
-    return out.tolist()
+    return np.max(x, axis = (1, 3)).tolist() 
